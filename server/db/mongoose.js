b274ecb61a3ef3fd73995b426c778c78e7f49a3f
Sagar <sagarthomas27@hotmail.com>
2018-09-02 00:54:02 -0400

    Add MONGO URI to heroku env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const API = require('./../../secrets');
 
 mongoose.Promise = global.Promise; //Sets the type of promises mongoose uses to built in one
-mongoose.connect('mongodb://localhost:27017/TodoApp' || API.MONGODB_URI );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
