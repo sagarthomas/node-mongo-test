339a05008e7b4b575fb7d6f86719c7516c96dd90
Sagar <sagarthomas27@hotmail.com>
2018-09-02 13:49:44 -0400

    Setup seperate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const API = require('./../../secrets');
 
 mongoose.Promise = global.Promise; //Sets the type of promises mongoose uses to built in one
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( process.env.MONGODB_URI);
 
 module.exports = {mongoose};
